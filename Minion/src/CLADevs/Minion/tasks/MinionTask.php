14c6619fd5e96235dd60baecd07b960877cf0ad5
aitester404 <42bzl42@gmail.com>
2025-11-20 06:29:01 +0300

    Update MinionTask.php

--- Minion/src/CLADevs/Minion/tasks/MinionTask.php
+++ Minion/src/CLADevs/Minion/tasks/MinionTask.php
@@ -2,18 +2,41 @@
 
 namespace CLADevs\Minion\tasks;
 
-use pocketmine\scheduler\Task;
-use CLADevs\Minion\MinionManager;
+use pocketmine\entity\Human;
+use pocketmine\entity\location\Location;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\player\Skin;
+use pocketmine\world\World;
 
-class MinionTask extends Task {
+class MinionTask extends Human {
 
-    private MinionManager $manager;
+    public const NETWORK_ID = Human::NETWORK_ID;
 
-    public function __construct(MinionManager $manager) {
-        $this->manager = $manager;
+    private int $tickCount = 0;
+
+    public function __construct(Location $location, Skin $skin, ?CompoundTag $nbt = null) {
+        parent::__construct($location, $nbt);
+        $this->setSkin($skin);
+        $this->setScale(0.66); // 2/3 boyut
+        $this->setNameTagAlwaysVisible(true);
+        $this->setNameTag("§bMinion");
+        $this->setImmobile(true); // Kendi başına yürüyemez
     }
 
-    public function onRun(): void {
-        $this->manager->tickMinions();
+    protected function initEntity(CompoundTag $nbt): void {
+        parent::initEntity($nbt);
+    }
+
+    public function onUpdate(int $currentTick): bool {
+        $this->tickCount++;
+        // Her 10 tickte kazma animasyonu
+        if($this->tickCount % 10 === 0){
+            $this->swingArm(); // Animasyon efekti
+        }
+        return parent::onUpdate($currentTick);
+    }
+
+    private function swingArm(): void {
+        $this->broadcastEntityEvent(Human::ARM_SWING);
     }
 }
