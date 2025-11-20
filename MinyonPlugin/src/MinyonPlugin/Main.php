a58e47d7af290e7e578a713069ea8b1424ca7d30
aitester404 <42bzl42@gmail.com>
2025-11-20 06:07:56 +0300

    Update Main.php

--- MinyonPlugin/src/MinyonPlugin/Main.php
+++ MinyonPlugin/src/MinyonPlugin/Main.php
@@ -6,20 +6,19 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use pocketmine\entity\EntityFactory;
 use pocketmine\world\World;
-use pocketmine\math\Vector3;
-use MinyonPlugin\entity\Minion;
-use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\entity\EntityDataHelper;
-use pocketmine\utils\TextFormat;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\entity\EntityFactory;
+use MinyonPlugin\entity\Minion;
 
 class Main extends PluginBase {
 
     public function onEnable(): void {
+
         EntityFactory::getInstance()->register(
             Minion::class,
-            function(World $world, CompoundTag $nbt): Minion {
+            function(World $world, CompoundTag $nbt) : Minion{
                 return new Minion(EntityDataHelper::parseLocation($nbt, $world), $nbt);
             },
             ['Minion']
@@ -28,17 +27,20 @@ class Main extends PluginBase {
         $this->getLogger()->info("MinyonPlugin aktif!");
     }
 
+
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+
         if(!$sender instanceof Player) return false;
 
         if($command->getName() === "minyon"){
-            $pos = $sender->getLocation();
 
-            $nbt = Minion::createBaseNBT($pos);
-            $minion = new Minion($pos, $nbt);
+            $loc = $sender->getLocation();
+            $nbt = Minion::createBaseNBT($loc);
+
+            $minion = new Minion($loc, $nbt);
             $minion->spawnToAll();
 
-            $sender->sendMessage(TextFormat::GREEN . "Minyon oluşturuldu!");
+            $sender->sendMessage("§aMinyon oluşturuldu!");
             return true;
         }
 
