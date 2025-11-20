7b6a0dd70a0e377609ee847bca75985ad84371c5
aitester404 <42bzl42@gmail.com>
2025-11-20 06:41:38 +0300

    Update MinionEntity.php

--- Minion/src/CLADevs/Minion/tasks/MinionEntity.php
+++ Minion/src/CLADevs/Minion/tasks/MinionEntity.php
@@ -3,7 +3,6 @@
 namespace CLADevs\Minion\tasks;
 
 use pocketmine\entity\Human;
-use pocketmine\player\Player;
 use pocketmine\player\Skin;
 use pocketmine\world\Position;
 use pocketmine\nbt\tag\CompoundTag;
@@ -14,7 +13,7 @@ class MinionEntity extends Human {
 
     public function __construct(Position $pos, Skin $skin, ?CompoundTag $nbt = null){
         parent::__construct($pos, $nbt);
-        $this->setSkin($skin); // PM5 player skin kullan
+        $this->setSkin($skin);
         $this->setScale(0.66); // 2/3 boyut
         $this->setNameTag("§bMinion");
         $this->setNameTagAlwaysVisible(true);
@@ -33,3 +32,4 @@ class MinionEntity extends Human {
         return parent::onUpdate($currentTick);
     }
 }
+
