5a5a9c6ce278f3094ed413ad01429b010656890a
aitester404 <42bzl42@gmail.com>
2025-11-20 06:11:36 +0300

    Update Minion.php

--- MinyonPlugin/src/MinyonPlugin/entity/Minion.php
+++ MinyonPlugin/src/MinyonPlugin/entity/Minion.php
@@ -7,42 +7,34 @@ use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\item\VanillaItems;
 use pocketmine\player\Player;
 use pocketmine\math\Vector3;
-use pocketmine\entity\EntityDataHelper;
-use pocketmine\utils\TextFormat;
 use MinyonPlugin\task\MinionMineTask;
 
 class Minion extends Human {
 
-    /** Human::$inventory ile çakışmaması için farklı isim */
     protected array $storage = [];
 
     public static function createBaseNBT($pos): CompoundTag {
-        return EntityDataHelper::createBaseNBT($pos);
+        return CompoundTag::create(); // ✔ PM5 için doğru yöntem
     }
 
     protected function initEntity(CompoundTag $nbt): void {
         parent::initEntity($nbt);
 
-        // Boy küçült
         $this->setScale(0.66);
 
-        // Eline pickaxe
         $this->getInventory()->setItemInHand(VanillaItems::DIAMOND_PICKAXE());
 
-        // Kazma görevini her 2 sn'de 1 tetikle
         $this->getServer()->getScheduler()->scheduleRepeatingTask(
             new MinionMineTask($this),
             40
         );
     }
 
-    /** Minyon envanterine item ekleme */
     public function addItem(string $id, int $count = 1): void {
         if(!isset($this->storage[$id])) $this->storage[$id] = 0;
         $this->storage[$id] += $count;
     }
 
-    /** PM5 doğru imza */
     public function onInteract(Player $player, Vector3 $clickPos): bool {
 
         $player->sendMessage("§b--- Minyon Envanteri ---");
@@ -55,7 +47,6 @@ class Minion extends Human {
         foreach($this->storage as $id => $count){
             $player->sendMessage("§a$id §fx§e$count");
         }
-
         return true;
     }
 }
